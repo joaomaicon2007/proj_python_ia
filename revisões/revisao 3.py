e27a9bc2f921be6ed59f4164b7d0dde011e32325
Joao <joaomichelcoelho@gmail.com>
2024-08-28 17:42:45 -0300

    Aula de revisão

---
+++ revisões/revisao 3.py
@@ -0,0 +1,9 @@
+idade = 16 
+if (idade>=18):
+    print("adulto")
+    else:
+        if(idade>=13):
+            print("adolecente")
+            else:
+                print("criança")
+                
