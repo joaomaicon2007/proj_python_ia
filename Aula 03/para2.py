bbd68eaff4145eec6fb030653040ef305bbe6397
Joao <joaomichelcoelho@gmail.com>
2024-08-28 18:58:20 -0300

    laço de repetisao

---
+++ Aula 03/para2.py
@@ -0,0 +1,19 @@
+alunos = [
+    "larissa"
+    "maria"
+    "caua"
+    "evelin"
+    "daniel"
+    "giovana"
+    "diogo"
+    "isadora"
+    "joao"
+    "pedro"
+]
+alunos.sort()
+alunos.append("larissa")
+alunos.reservar()
+y = 1
+for x in alunos 
+    print(y,"-",x)
+    y=y+1
